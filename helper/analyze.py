b9c0a6d766747c8d52f3f544364cf05ba511bb1e
Shane Drabing <shane.drabing@gmail.com>
2021-05-14 14:38:38 -0600

    docstrings for analysis.py

--- helper/analyze.py
+++ helper/analyze.py
@@ -24,29 +24,52 @@ PALETTE_TABLEAU = (
 # FUNCTIONS
 
 
-def palette(x):
+def palette(n):
+    """Return corresponding color for a given index (mod div into range)
+
+    :param n: Integer
+    :returns: Pyplot named color
+    """
     return nest(
         part(mod, len(PALETTE_TABLEAU)),
         get(PALETTE_TABLEAU, iskey=False)
-    )(x)
+    )(n)
 
 
 def convert(dct):
+    """Convert a dict of str values to numeric where possible
+
+    :param dct: dict with strs from cleaned data
+    :returns: dict with numeric data
+    """
     return dict(zip(dct.keys(), pipe(safenum)(dct.values())))
 
 
 def todatadict(data):
+    """Reorganize tuple of dicts into single dict where keys are defined by ID
+    and NAME, values are numerical data corresponding to RPKM values
+
+    :param data: tuple of dicts from loaded cleaned data
+    :returns: dict containing reorganized data
+    """
     full = nest(get(0), dict.keys, tuple)(data)
     k = nest(filt(nest(VALID_KEYS.__contains__, lambda x: not x)), tuple)(full)
     dct = {
         (x["ID"], x["NAME"]): pipe(get(x, iskey=False))(k)
         for x in data
     }
+
+    # remember the keys associated with the RPKM values
     dct[None] = k
     return dct
 
 
 def load(fname):
+    """Load from cleaned data
+
+    :param fname: Filename of cleaned data
+    :returns: dict containing reorganized data
+    """
     with open(fname) as f:
         return nest(
             csv.DictReader,
@@ -56,6 +79,12 @@ def load(fname):
 
 
 def error(a, b):
+    """Calculate SD between two numerical containers
+
+    :param a: tuple of RPKM values
+    :param b: tuple of RPKM values
+    :returns: Standard deviation
+    """
     return nest(
         apply(nest(sub, part(pow, 2))),
         sum,
@@ -64,6 +93,12 @@ def error(a, b):
 
 
 def kstep(means, values):
+    """Assign values (RPKM) to the mean with minimal error
+
+    :param means: K-means
+    :param values: tuples of RPKM values
+    :returns: Sum within group error, assignments, and original means
+    """
     sse = 0
     labels = tuple()
     for x in values:
@@ -74,6 +109,12 @@ def kstep(means, values):
 
 
 def mstep(labels, values):
+    """Generate new k-means given labeled RPKM values
+
+    :param labels: K-mean assignments
+    :param values: tuples of RPKM values
+    :returns: New k-means
+    """
     dct = collections.defaultdict(list)
     for i, x in zip(labels, values):
         dct[i].append(x)
@@ -82,6 +123,16 @@ def mstep(labels, values):
 
 
 def plot_kmeans(fname, means, labels, values, xlabs, scaling):
+    """Pyplot rendering of k-means with all original RPKM profiles as well
+
+    :param fname: Output filename (e.g. "plot.png")
+    :param means: K-means
+    :param labels: K-mean assignments
+    :param values: tuples of RPKM values
+    :param xlabs: Labels for the X-axis (tissue names, typically)
+    :param scaling: Identity, zscore, or min-max scaling
+    :returns: None
+    """
     for i, x in zip(labels, values):
         plt.plot(x, color=palette(i), linewidth=0.1)
 
@@ -109,6 +160,18 @@ def plot_kmeans(fname, means, labels, values, xlabs, scaling):
 
 
 def analyze(inp_fname, out_fname, plt_fname=None, k=3, thresh_init=1e-2, thresh_conv=1e-4, scaling="identity"):
+    """Given cleaned data (output from `clean` function), run k-means
+    clustering analysis, outputting textual and optionally plot output
+
+    :param inp_fname: Input filename of raw data
+    :param out_fname: Output filename for k-means data
+    :param plt_fname: (optional) Output filename for k-means plot
+    :param k: (optional) Number of k-means
+    :param thresh_init: (optional) Threshold for initiation
+    :param thresh_conv: (optional) Threshold for convergence
+    :param scaling: (optional) Identity, zscore, or min-max scaling
+    :returns: None
+    """
     scaling_funcs = {
         "identity": identity,
         "zscore": zscore,
